43c13cc7f6ec5518049bf582175d2bfdc8e40ae2
augustoserra-edu <augusto.serra@comunidad.ub.edu.ar>
2026-03-18 21:20:49 -0300

    Add files via upload

---
+++ ejercicios_Clase2.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 21:05:49 2026
+
+@author: Alumnos
+"""
+
+"---------------------------------1-------------------------------------------"
+"""
+def calcular_area(base,altura):
+    
+    area = base * altura
+    
+    print(area)
+    return area
+
+calcular_area(5,10)
+"""
+
+
+"---------------------------------2-------------------------------------------"
+"""
+    try:
+        conectar()
+    except Exception:
+        pass
+"""
+"---------------------------------3-------------------------------------------"
+"""
+def saludo():
+    userName = input("introduzca su usuario: ")
+    print(f"Saludos {userName}")
+    
+saludo()    
+
+"""
+
+"---------------------------------4-------------------------------------------"
